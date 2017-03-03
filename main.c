63430331ad8c9979775541893189779c77d80168
Kanister10l <kamrep7@wp.pl>
2017-03-03 22:41:41 +0100

    Base

--- main.c
+++ main.c
@@ -12,11 +12,11 @@ int main(){
     conf config = malloc(sizeof * config);
     readConfig(config);
     gen life = createMatrix(100,100,0);
-    createRandomGeneration(life, 0.3);
+    createRandomGeneration(life, config->probability);
     gen *lifes = simulateAllGenerations(life, config->gifAfterYears);
     for (int i = 0; i < lifes[99]->x; ++i) {
         for (int j = 0; j < lifes[99]->y; ++j) {
-            printf("%d", lifes[5]->matrix[i][j]);
+            printf("%d", lifes[0]->matrix[i][j]);
         }
         printf("\n");
     }
